966856b291d0d3724abcd255e336aca0a6a52638
Ammar Haddadi <75220043+AmmarHaddadi@users.noreply.github.com>
2023-03-14 11:26:32 +0100

    .

--- Base-x.sh
+++ Base-x.sh
@@ -3,7 +3,7 @@ name=""
 password=""
 hostname=""
 # makes a partition 1 for /boot, and 2 for the rest
-(echo o; echo n; echo p; echo 1; echo ""; echo +300M; echo n; echo p; echo 2; echo ""; echo ""; echo a; echo 1;echo p; echo w; echo q) | fdisk /dev/sda
+(echo o; echo n; echo p; echo 1; echo ""; echo +300M; echo n; echo p; echo 2; echo ""; echo ""; echo a; echo 1;echo p; echo w) | fdisk /dev/sda
 
 # for VMs, sda should be replaced with vda, in vim do :%s/sda/vda/g
 mkfs.ext4 -L ROOT /dev/sda2 
@@ -15,8 +15,12 @@ mount /dev/disk/by-label/ROOT /mnt
 mkdir -pv /mnt/boot/efi
 mount /dev/disk/by-label/ESP /mnt/boot/efi
 
-timedatectl set-ntp true 
+#time
+ntpd -qg
 rc-service ntpd start
+rc-update add ntpd default
+
+#necessery stuff
 basestrap /mnt base base-devel linux linux-firmware openrc elogind-openrc vim
 fstabgen -U /mnt >> /mnt/etc/fstab
 artix-chroot /mnt
@@ -35,11 +39,12 @@ LC_TIME=en_US.UTF-8" > /etc/locale.conf
 pacman-key --init
 # pacman-key --populate archlinux
 pacman-key --refresh-keys
+pacman -S sed
 sed -i 's/^#Color/Color/' /etc/pacman.conf
 sed -i '/^Color/a ParallelDownloads = 8' /etc/pacman.conf
 
 #install and configure boot loader
-pacman -S grub efibootmgr sudo
+pacman -S --needed grub efibootmgr
 grub-install --recheck /dev/sda --target=x86_64-efi --bootloader-id=grub_uefi 
 grub-mkconfig -o /boot/grub/grub.cfg #for UEFI system
 
@@ -62,7 +67,7 @@ ff02::2    ip6-allrouters
 # This host address
 127.0.1.1  "$name"
 " > /etc/hosts
-echo "hostname='$hostname'" > /etc/conf.d/hostname
+echo "hostname=$hostname" > /etc/conf.d/hostname
 pacman -S dhcpcd networkmanager networkmanager-openrc
 rc-update add networkmanager
 
